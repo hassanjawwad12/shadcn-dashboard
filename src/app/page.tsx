86145425d99a5c3e8adc6c221e5c8aa4343926ce
Hassan Jawwad <hassan.jawwad@datumbrain.com>
2025-01-07 17:10:19 +0500

    ⚡Add Grid on Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,19 @@ import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
-   <div className="grid grid-cols-2 gap-[32px]">
-    <General/>
-    <div className="grid grid-cols-2 gap-[32px]">
-    <Card/>
-    <Card/>
+    <div className="grid gap-[32px]">
+      <div className="grid grid-cols-2 gap-[32px]">
+        <General />
+        <div className="grid grid-rows-2 gap-[32px]">
+          <Card />
+          <Card />
+        </div>
+      </div>
+      <div className="grid grid-cols-3 gap-[32px]">
+      <Card className="h-[300px]">j</Card>
+      <Card className="h-[300px]">k</Card>
+      <Card className="h-[300px]">j</Card>
+      </div>
     </div>
-   </div>
   );
 }
